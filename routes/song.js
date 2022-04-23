48337451ade69f54f497fe6ff5c2b71be6e221fa
Jotaeme55 <60939740+Jotaeme55@users.noreply.github.com>
2022-04-23 18:13:10 +0200

    algunas nuevas routas de backend

--- routes/song.js
+++ routes/song.js
@@ -3,47 +3,138 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 const song = require('../models/song');
-const fs= require("fs")
+const fs = require("fs")
 
 //get all songs
-router.get('',async(req, res) => {
-        try {
-            const songDB = await song.find();
-            res.status(200).json(songDB);
-        } catch (error) {
-            console.log(error)
-            return res.status(400).json({
+router.get('/', async (req, res) => {
+    try {
+        const songDB = await song.find();
+        res.status(200).json(songDB);
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({
             mensaje: 'An error has occurred',
             error
-            })
-        }
-    })
+        })
+    }
+})
+
+//get all songs and order by year released
+router.get('/orderByAddedDescendant', async (req, res) => {
+    try {
+        const songDB = await song.find().sort({ added:-1});
+        res.status(200).json(songDB);
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({
+            mensaje: 'An error has occurred',
+            error
+        })
+    }
+})
+
+//get all songs and order by top year
+router.get('/orderByYearReleasedAscendant', async (req, res) => {
+    try {
+        const songDB = await song.find().sort({year_released:1});
+        res.status(200).json(songDB);
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({
+            mensaje: 'An error has occurred',
+            error
+        })
+    }
+})
 
 //Get Song by Id
-router.get('/:songId', async(req, res) => {
+router.get('/:songId', async (req, res) => {
     const _Id = req.params.songId;
-        try {
-            const songDB = await song.findOne({"_id": mongoose.Types.ObjectId(_Id)});
-            res.status(200).json(songDB);
-        } catch (error) {
-            return res.status(400).json({
+    try {
+        const songDB = await song.findOne({ "_id": mongoose.Types.ObjectId(_Id) });
+        res.status(200).json(songDB);
+    } catch (error) {
+        return res.status(400).json({
             mensaje: 'An error has occurred',
             error
-            })
-        }
-    });
-//delete song by id
-router.delete('/:songId',async(req, res) => {
-    const _Id = req.params.songId;
-        try {
-            const songDB = await song.findOneAndRemove({"_id": mongoose.Types.ObjectId(_Id)});
-            res.status(200).json(songDB);
-        } catch (error) {
-            return res.status(400).json({
-            mensaje: 'An error has occurred',
-            error
-            })
-        }
-    })
+        })
+    }
+});
 
-    module.exports = router;
+//delete song by id
+router.delete('/:songId', async (req, res) => {
+    const _Id = req.params.songId;
+    try {
+        const songDB = await song.findOneAndRemove({ "_id": mongoose.Types.ObjectId(_Id) });
+        res.status(200).json(songDB);
+    } catch (error) {
+        return res.status(400).json({
+            mensaje: 'An error has occurred',
+            error
+        })
+    }
+})
+
+//crete a song
+router.post("/", async (req, res) => {
+    const body = req.body
+    body._id = new mongoose.Types.ObjectId()
+    try {
+        var songDB = await song.create(body)
+        res.status(200).json(songDB)
+    } catch (error) {
+        return res.status(500).json({
+            mensaje: "error in posting song",
+            error: error
+        })
+    }
+})
+
+//update a song
+router.put("/:songId",async (req, res) => {
+    const body = req.body
+    body._id = req.params.songId
+    try {
+        var songDB = await song.findOneAndUpdate(body)
+        res.status(200).json(songDB)
+    } catch (error) {
+        return res.status(500).json({
+            mensaje: "error in posting song",
+            error: error
+        })
+    }
+})
+
+//get songs by title
+router.get('/title/:title', async (req, res) => {
+    const title = req.params.title;
+    try {
+        const songDB = await song.find({ "title": title});
+        res.status(200).json(songDB);
+    } catch (error) {
+        return res.status(400).json({
+            mensaje: 'An error has occurred',
+            error
+        })
+    }
+})
+
+//get songs by artist
+router.get('/artist/:artist', async (req, res) => {
+    const artist = req.params.artist;
+    try {
+        const songDB = await song.find({ "artist": artist});
+        res.status(200).json(songDB);
+    } catch (error) {
+        return res.status(400).json({
+            mensaje: 'An error has occurred',
+            error
+        })
+    }
+})
+
+
+
+
+
+module.exports = router;
